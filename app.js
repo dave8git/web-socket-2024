87757b16a38f27992e5b01686d3e8b79f424830f
dave8git <dave8git@gmail.com>
2024-10-09 04:37:39 +0200

    Add messages are being added correctly for one user

--- app.js
+++ app.js
@@ -9,13 +9,42 @@ const messageContentInput = document.querySelector('#message-content');
 loginForm.addEventListener('submit', login);
 
 function login(e) {
-    e.preventDefault(); // Prevent form submission
+    e.preventDefault();
 
-    const enteredUsername = userNameInput.value.trim(); // Get trimmed input value
+    const enteredUsername = userNameInput.value.trim();
     if (enteredUsername !== '') {
-        username = enteredUsername;  // Set global username variable
-        alert(`Correct username entered: ${username}`);
+        username = enteredUsername;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
     } else {
-        alert('Please enter a username');  // Show alert if no username entered
+        alert('Please enter a username');
     }
 }
+
+addMessagesForm.addEventListener('submit', sendMessage);
+
+function sendMessage(e) {
+    e.preventDefault();
+
+    const enteredMessage = messageContentInput.value.trim();
+    if (enteredMessage !== '') {
+        addMessage(username, enteredMessage);
+    } else {
+        alert('Wpisz treść wiadomości.')
+    }
+}
+
+function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === username) message.classList.add('message--self');
+    message.innerHTML = `
+        <h3 class="message__author">${username == author ? 'You' : author}</h3>
+        <div class="message__content">
+            ${content}
+        </div>
+    `;
+    messagesList.appendChild(message);
+}
+
